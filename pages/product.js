0d280b34bda41612193c6965ed6ca133ab57f10d
Lawrence <Caulker>
2020-05-11 08:45:23 -0400

    completed product-detail functionality

--- pages/product.js
+++ pages/product.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-function Product() {
+function Product({ product }) {
+  console.log({ product })
   return <>product</>
 }
 
@@ -8,6 +9,8 @@ Product.getInitialProps = async ({ query: { _id } }) => {
   const url = "http://localhost:3000/api/product"
   const payload = { params: { _id } }
   const response = await axios.get(url, payload)
+
+  return { product: response.data }
 }
 
 export default Product
