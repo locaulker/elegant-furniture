0304c9c67a4cee4616f66f81c53d6160348d6070
Lawrence <locaulker@gmail.com>
2020-05-12 02:33:39 -0400

    added form upload functionality

--- pages/create.js
+++ pages/create.js
@@ -9,6 +9,8 @@ import {
   Header,
   Icon,
 } from "semantic-ui-react"
+import axios from "axios"
+import baseUrl from "../utils/baseUrl"
 
 const INITIAL_PRODUCT = {
   name: "",
@@ -21,6 +23,7 @@ function CreateProduct() {
   const [product, setProduct] = React.useState(INITIAL_PRODUCT)
   const [mediaPreview, setMediaPreview] = React.useState("")
   const [success, setSuccess] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
 
   function handleChange(event) {
     const { name, value, files } = event.target
@@ -33,9 +36,26 @@ function CreateProduct() {
     }
   }
 
-  function handleSubmit(event) {
+  async function handleImageUpload() {
+    const data = new FormData()
+    data.append("file", product.media)
+    data.append("upload_preset", "elegant-furniture")
+    data.append("cloud_name", "djbn8sdhz")
+    const response = await axios.post(process.env.CLOUDINARY_URL, data)
+    const mediaUrl = response.data.url
+    return mediaUrl
+  }
+
+  async function handleSubmit(event) {
     event.preventDefault()
-    console.log(product)
+    setLoading(true)
+    const mediaUrl = await handleImageUpload()
+    console.log({ mediaUrl })
+    const url = `${baseUrl}/api/product`
+    const payload = { ...product, mediaUrl }
+    const response = await axios.post(url, payload)
+    console.log({ response })
+    setLoading(false)
     setProduct(INITIAL_PRODUCT)
     setSuccess(true)
   }
@@ -47,7 +67,7 @@ function CreateProduct() {
         Create New Product
       </Header>
 
-      <Form success={success} onSubmit={handleSubmit}>
+      <Form loading={loading} success={success} onSubmit={handleSubmit}>
         <Message
           success
           icon="check"
@@ -95,6 +115,7 @@ function CreateProduct() {
         />
         <Form.Field
           control={Button}
+          disabled={loading}
           color="blue"
           icon="pencil alternate"
           content="Submit"
